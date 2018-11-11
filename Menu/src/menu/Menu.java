7b11dd74ef63afe7b828772665f8391653fc2a7e
Matias Narvaja <41093723+MatiasNarvaja@users.noreply.github.com>
2018-11-10 21:28:44 -0300

    Create Menu

---
+++ Menu/src/menu/Menu.java
@@ -0,0 +1,15 @@
+
+package menu;
+
+
+public class Menu {
+
+    
+    public static void main(String[] args) {
+           Condiciones window = new Condiciones();
+        window.setTitle("Menu");
+        window.setVisible(true);
+        window.setLocationRelativeTo(null);
+    }
+    
+}
